9978ff84e88773d428ddc555607016b9d5a74419
Dan Stefaniuk <daniel.stefaniuk@gmail.com>
2022-03-24 22:48:32 +0100

    Do not include the test suite if the scripts only flag is set

--- install.sh
+++ install.sh
@@ -50,6 +50,7 @@ function download() {
     /tmp/$PROJECT_NAME*
   if [[ "$SCRIPTS_ONLY" =~ ^(true|yes|y|on|1|TRUE|YES|Y|ON)$ ]]; then
     rm /tmp/$ORG_NAME-$REPO_NAME-*/scripts/makeops/$REPO_NAME/init.mk
+    find /tmp/$ORG_NAME-$REPO_NAME-*/scripts/makeops/$REPO_NAME -type f -name '*.test.sh' -exec rm -rf {} \;
     mkdir -p "$INSTALL_DIR"
     cp -Rf /tmp/$ORG_NAME-$REPO_NAME-*/scripts/* "$INSTALL_DIR"
     rm -rf /tmp/$ORG_NAME-$REPO_NAME-*
